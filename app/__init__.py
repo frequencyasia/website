506c0fefdc2f7a78b4532496c7ea35066f6909e6
Luke Hansford <luke@klinify.com>
2015-11-30 15:25:01 +0800

    Added db stuff and backbone app.

--- app/__init__.py
+++ app/__init__.py
@@ -1,8 +1,15 @@
 from flask import Flask
 from flask.ext.sqlalchemy import SQLAlchemy
+from flask_admin import Admin
+from flask_admin.contrib.sqla import ModelView
 
 app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
 from app import views, models
+
+# ADMIN STUFF
+admin = Admin(app, name='microblog', template_mode='bootstrap3')
+admin.add_view(ModelView(models.Show, db.session))
+admin.add_view(ModelView(models.Episode, db.session))
--- app/views.py
+++ app/views.py
@@ -1,10 +1,9 @@
+from flask import render_template
+
 from app import app
+from models import Show
 
 @app.route('/')
 @app.route('/index')
 def index():
-    return render_template('index.html', title="Frequency Asia")
-
-@app.route("/shows")
-def shows():
-    return render_template('shows.html', title="Frequency Asia")
+    return render_template('index.html')
