c3fd8c330918125e4440839e3aad777fadb86a72
Luke Hansford <luke@klinify.com>
2015-12-28 18:47:40 +0800

    Added social links

--- js_app/js/views/showView.js
+++ js_app/js/views/showView.js
@@ -24,7 +24,7 @@ module.exports = Backbone.View.extend({
         }
         this.showData = data;
         this.render();
-        document.title = this.showData.name + "| Frequency Asia";
+        document.title = this.showData.name + " | Frequency Asia";
       });
   },
 
