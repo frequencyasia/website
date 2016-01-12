3e5f8923b9fc2393979940cd303ff837d933c368
Luke Hansford <luke@klinify.com>
2016-01-12 08:17:01 +0800

    Added logging

--- app/__init__.py
+++ app/__init__.py
@@ -12,6 +12,14 @@ app = Flask(__name__)
 app.config.from_object('config')
 db = SQLAlchemy(app)
 
+if not app.debug:
+    import logging
+    from logging.handlers import RotatingFileHandler
+    file_handler = RotatingFileHandler('python.log', maxBytes=1024 * 1024 * 100, backupCount=20)
+    file_handler.setLevel(logging.ERROR)
+    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    file_handler.setFormatter(formatter)
+    app.logger.addHandler(file_handler)
 
 # Create directory for file fields to use
 file_path = os.path.join(os.path.dirname(__file__), 'static', 'files')
