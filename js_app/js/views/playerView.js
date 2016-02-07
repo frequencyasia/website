bbb7ea3e4c9a55be3d06433aa3c9aec827fdc511
Luke Hansford <luke@klinify.com>
2016-02-07 14:02:16 +0800

    Fixes issue when no url.

--- js_app/js/views/playerView.js
+++ js_app/js/views/playerView.js
@@ -58,9 +58,9 @@ module.exports = Backbone.View.extend({
     $.getJSON("http://airtime.frequency.asia/api/live-info")
       .done(function(data) {
         if (data && data.current) {
-          if (data.current.name.length && data.current.url.length) {
+          if (data.current.url && data.current.name && data.current.name.length && data.current.url.length) {
             setNowPlaying('<a href="' + data.current.url + '">' + data.current.name + '</a>');
-          } else if (data.current.name.length) {
+          } else if (data.current.name && data.current.name.length) {
             setNowPlaying(data.current.name);
           } else {
             setNowPlaying('Offline', true);
