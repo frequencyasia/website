b809d542321c1b78cb94ecc9607a3b40ba8e35d4
Luke Hansford <luke@klinify.com>
2016-05-22 15:44:02 +0800

    Update production build script.

--- build.sh
+++ build.sh
@@ -6,5 +6,6 @@ npm install
 
 gulp build-production
 
-cp -rf ./dist/bundle.js /var/www/api-production/app/static/dist/bundle.js
-cp -rf ./dist/styles.min.css /var/www/api-production/app/static/dist/styles.min.css
+cp -rf /var/www/website-production/dist/bundle.js /var/www/api-production/app/static/dist/bundle.js
+cp -rf /var/www/website-production/dist/bundle.js.map /var/www/api-production/app/static/dist/bundle.js.map
+cp -rf /var/www/website-production/dist/styles.min.css /var/www/api-production/app/static/dist/styles.min.css
