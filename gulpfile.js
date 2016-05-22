86afb6f7faf8a619025713c16ba86bd16e3d6a5b
Luke Hansford <luke@klinify.com>
2016-05-22 13:54:25 +0800

    Gulp refactoring.

--- gulpfile.js
+++ gulpfile.js
@@ -13,70 +13,71 @@ var rename = require('gulp-rename');
 var postcss = require('gulp-postcss');
 var notify = require('gulp-notify');
 var uglify = require('gulp-uglify');
-var doiuse = require('doiuse');
+//
+// // add custom browserify options here
+// var customOpts = {
+//   entries: ['./js/app.js'],
+//   debug: true,
+//   transform: [brfs, [babelify, {presets: ["es2015"]}]],
+// };
+// var b = browserify(customOpts);
+//
+// gulp.task('watch-js', bundleAndWatch);
+// gulp.task('js', bundle);
+//
+// function bundleAndWatch() {
+//   var watchify = require('watchify');
+//   var opts = assign({}, watchify.args, customOpts);
+//   var w = watchify(browserify(opts));
+//   w.on('update', bundle); // on any dep update, runs the bundler
+//   w.on('log', gutil.log); // output build logs to terminal
+//   return w.bundle()
+//     // log errors if they happen
+//     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+//     .pipe(source('bundle.js'))
+//     // optional, remove if you don't need to buffer file contents
+//     .pipe(buffer())
+//     // optional, remove if you dont want sourcemaps
+//     .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+//     .pipe(uglify())
+//        // Add transformation tasks to the pipeline here.
+//     .pipe(sourcemaps.write('./')) // writes .map file
+//     .pipe(gulp.dest('./dist'));
+// }
+//
+// function bundle() {
+//   return b.bundle()
+//     // log errors if they happen
+//     .on('error', gutil.log.bind(gutil, 'Browserify Error'))
+//     .pipe(source('bundle.js'))
+//     // optional, remove if you don't need to buffer file contents
+//     .pipe(buffer())
+//     // optional, remove if you dont want sourcemaps
+//     .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
+//     .pipe(uglify())
+//        // Add transformation tasks to the pipeline here.
+//     .pipe(sourcemaps.write('./')) // writes .map file
+//     .pipe(gulp.dest('./dist'));
+// }
 
-// add custom browserify options here
-var customOpts = {
-  entries: ['./js/app.js'],
-  debug: true,
-  transform: [brfs, [babelify, {presets: ["es2015"]}]],
-};
-var b = browserify(customOpts);
-
-gulp.task('watch-js', bundleAndWatch);
-gulp.task('js', bundle);
-
-function bundleAndWatch() {
-  var watchify = require('watchify');
-  var opts = assign({}, watchify.args, customOpts);
-  var w = watchify(browserify(opts));
-  w.on('update', bundle); // on any dep update, runs the bundler
-  w.on('log', gutil.log); // output build logs to terminal
-  return w.bundle()
-    // log errors if they happen
-    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-    .pipe(source('bundle.js'))
-    // optional, remove if you don't need to buffer file contents
-    .pipe(buffer())
-    // optional, remove if you dont want sourcemaps
-    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-    .pipe(uglify())
-       // Add transformation tasks to the pipeline here.
-    .pipe(sourcemaps.write('./')) // writes .map file
+gulp.task('build-js', function() {
+  return browserify({
+    entries: ['./js/app.js'],
+    debug: isProduction,
+  }).transform(babelify, { presets: ['es2015', 'react'] })
+    .transform('brfs')
+    .bundle()
+    .pipe(source('bundle.js')) // gives streaming vinyl file object
+    .pipe(buffer()) // <----- convert from streaming to buffered vinyl file object
+    .pipe(uglify()) // now gulp-uglify works
     .pipe(gulp.dest('./dist'));
-}
-
-function bundle() {
-  return b.bundle()
-    // log errors if they happen
-    .on('error', gutil.log.bind(gutil, 'Browserify Error'))
-    .pipe(source('bundle.js'))
-    // optional, remove if you don't need to buffer file contents
-    .pipe(buffer())
-    // optional, remove if you dont want sourcemaps
-    .pipe(sourcemaps.init({loadMaps: true})) // loads map from browserify file
-    .pipe(uglify())
-       // Add transformation tasks to the pipeline here.
-    .pipe(sourcemaps.write('./')) // writes .map file
-    .pipe(gulp.dest('./dist'));
-}
+});
 
 gulp.task('postcss', function() {
   // Runs postCSS with all its plugins.
   var nano = require('gulp-cssnano');
   var plugins = [
     require('autoprefixer'),
-    doiuse({
-      browsers: [
-        'ie >= 8',
-        '> 1%'
-      ],
-      // ignore: ['vh'], // an optional array of features to ignore
-      // ignoreFiles: ['**/normalize.css'], // an optional array of file globs to match against original source file path, to ignore
-      onFeatureUsage: function (usageInfo) {
-        console.log(usageInfo.message)
-      }
-    }),
   ];
   // Run PostCSS
   return gulp.src('./stylesheets/styles.css')
@@ -94,19 +95,19 @@ gulp.task('postcss', function() {
       }());
 });
 
-gulp.task('watch-styles', function watchStyles() {
-  // Watches the stylesheet folders for changes and runs the 'postcss' task if a change occurs.
-  gulp.watch(['./stylesheets/*.css'], ['postcss']);
-  return;
-});
+// gulp.task('watch-styles', function watchStyles() {
+//   // Watches the stylesheet folders for changes and runs the 'postcss' task if a change occurs.
+//   gulp.watch(['./stylesheets/*.css'], ['postcss']);
+//   return;
+// });
+//
+// gulp.task("default", [
+//   'postcss',
+//   'watch-styles',
+//   "watch-js",
+// ]);
 
-gulp.task("default", [
+gulp.task('build', [
   'postcss',
-  'watch-styles',
-  "watch-js",
-]);
-
-gulp.task("build", [
-  'postcss',
-  "js",
+  'build-js',
 ]);
