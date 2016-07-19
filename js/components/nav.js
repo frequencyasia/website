1443295edb0713fadcd6d384891267268c9a216d
Luke Hansford <luke@klinify.com>
2016-07-19 08:23:19 +0800

    Add link highlighting in nav

--- js/components/nav.js
+++ js/components/nav.js
@@ -7,6 +7,19 @@ module.exports = React.createClass({
     nowPlayingUrl: React.PropTypes.string.isRequired,
   },
 
+  isSelected: function isSelected(path) {
+    // Return appropriate class if given link is the current route.
+    console.log(window.location)
+    if (path === 'nowPlaying' && window.location.pathname === 'nowPlayingUrl') {
+      return 'c-nav__item--active';
+    } else if (window.location.pathname !== 'nowPlayingUrl') { // Need this condition because show and nowplaying will both match otherwise.
+      const pathArray = window.location.pathname.split('/');
+      if (pathArray[0] === path) {
+        return 'c-nav__item--active';
+      }
+    }
+  },
+
   render: function render() {
     let nowPlayingLabel = i18next.t('home').toLowerCase();
     if (this.props.nowPlayingUrl !== '/') {
@@ -15,12 +28,12 @@ module.exports = React.createClass({
     return (
       <nav className="c-nav c-header__content" role="navigation">
         <Link className="u-no-border u-no-padding" href="/"><div className="c-header__content__logo">{ i18next.t('home').toLowerCase() }</div></Link>
-        <Link href={ this.props.nowPlayingUrl } ><div className="c-nav__item">{ nowPlayingLabel }</div></Link>
-        <Link href="/shows"><div className="c-nav__item">{ i18next.t('shows').toLowerCase() }</div></Link>
-        <Link href="/schedule"><div className="c-nav__item">{ i18next.t('schedule').toLowerCase() }</div></Link>
-        <Link href="/wiki"><div className="c-nav__item">{ i18next.t('wiki').toLowerCase() }</div></Link>
-        <Link href="/projects"><div className="c-nav__item">{ i18next.t('projects').toLowerCase() }</div></Link>
-        <Link href="/about"><div className="c-nav__item">{ i18next.t('about').toLowerCase() }</div></Link>
+        <Link href={ this.props.nowPlayingUrl } ><div className="c-nav__item {this.isSelected('nowPlaying')}">{ nowPlayingLabel }</div></Link>
+        <Link href="/shows"><div className="c-nav__item {this.isSelected('shows')}">{ i18next.t('shows').toLowerCase() }</div></Link>
+        <Link href="/schedule"><div className="c-nav__item {this.isSelected('schedule')}">{ i18next.t('schedule').toLowerCase() }</div></Link>
+        <Link href="/wiki"><div className="c-nav__item {this.isSelected('wiki')}">{ i18next.t('wiki').toLowerCase() }</div></Link>
+        <Link href="/projects"><div className="c-nav__item {this.isSelected('projects')}">{ i18next.t('projects').toLowerCase() }</div></Link>
+        <Link href="/about"><div className="c-nav__item {this.isSelected('about')}">{ i18next.t('about').toLowerCase() }</div></Link>
       </nav>
     );
   },
